edcbb95e5872c645992d10267ff1210d3c068aea
Kamalov-Q <kamolovquvomiddin9@gmail.com>
2026-03-30 05:38:34 +0500

    working on tutorial problems

--- tutorial/main.c
+++ tutorial/main.c
@@ -1,29 +1,63 @@
 #include<stdio.h>
 #include<string.h>
+#include<math.h>
 
-void happyBday(char name[], int age) {
-    printf("\nHappy birthday to you!\n");
-    printf("\nHappy birthday to you!\n");
-    printf("\nHappy birthday to you!\n");
-    printf("\nHappy birthday dear %s!\n", name);
-    printf("\nYou are %d years old!\n", age);
+float square(float num) {
 
+    float result = num *num;
+    return result;
 
-};
+}
+
+float cubic(float num) {
+
+    float result = num * num *num;
+    return result;
+}
+
+float calculate(float num1, float num2) {
+
+    float result = num1 * num2;
+    return result;
+
+}
+
 
 int main() {
+    int choice;
+    float x, y;
 
-    char name[50];
-    int age;
+    printf("Choose 1-3\n");
+    printf("1.Find the square !\n");
+    printf("2.Find the cub !\n");
+    printf("3.Multiply 2 numbers !\n");
 
-    printf("Enter your name : ");
-    fgets(name, sizeof(name), stdin);
-    name[strlen(name) - 1] = '\0';
+    printf("Which one do u choose : ");
+    scanf("%d", &choice);
 
-    printf("Enter your age : ");
-    scanf("%d", &age);
+    switch(choice) {
+        case 1: 
+            printf("Enter a value to find square: ");
+            scanf("%f", &x);
+            printf("The square of %.1f is %.1f\n", x, square(x));
+            break;
 
-    happyBday(name, age);
+        case 2: 
+            printf("Enter a value to find the cub : ");
+            scanf("%f", &x);
+            printf("The cub of %.1f is %.1f\n", x, cubic(x));
+            break;
+
+        case 3: 
+            printf("Enter values for x and y to multiply: ");
+            scanf("%f", &x);
+            scanf("%f", &y);
+            printf("The result of multiplication of (%.1fx%.1f) is %.1f\n", x, y, calculate(x, y));
+            break;
+
+        default:
+            printf("Enter a proper choice (1-3)\n");
+    }
 
 
     return 0;
